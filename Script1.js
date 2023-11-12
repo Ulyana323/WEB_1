09eaaf1dfbbd1997d62fcf91fc35cb0d9d73561a
Ulyana323 <ulya2033@gmail.com>
2023-11-12 20:38:46 +0300

    scr4

--- Script1.js
+++ Script1.js
@@ -121,22 +121,56 @@ const target = document.querySelector('.cont1');
 observer.observe(target);
 
 let page = 1;
+let Page = 0;
 
 function fetchData() {
+    Page++;
+    if (Page > 5) { return; }
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
             .then(data => {
                 // Обработать полученные данные и добавить их на страницу
-                for (let i = 0; i < 5; i++) {
-                  
-                    if (i == 4) { appendDataToPageL(data[i]); break; }
-                    appendDataToPage(data[i]);
+                switch (Page) {
+                    case 1:
+                        for (let i = 0; i < 20; i++) {
+
+                            if (i == 19) { appendDataToPageL(data[i]); break; }
+                            appendDataToPage(data[i]);
+                        }
+                        break;
+                    case 2:
+                        for (let i = 20; i < 40; i++) {
+
+                            if (i == 39) { appendDataToPageL(data[i]); break; }
+                            appendDataToPage(data[i]);
+                        }
+                        break;
+                    case 3:
+                        for (let i = 40; i < 60; i++) {
+
+                            if (i == 59) { appendDataToPageL(data[i]); break; }
+                            appendDataToPage(data[i]);
+                        }
+                        break;
+                    case 4:
+                        for (let i = 60; i <80 ; i++) {
+
+                            if (i == 79) { appendDataToPageL(data[i]); break; }
+                            appendDataToPage(data[i]);
+                        }
+                        break;
+                    case 5:
+                        for (let i = 80; i <100; i++) {
+
+                            if (i == 99) { appendDataToPageL(data[i]); break; }
+                            appendDataToPage(data[i]);
+                        }
+                        break;
                 }
-                
             });
 }
 function appendDataToPage(data) {
-    if (page > 4) { return; }
+    if (page > 6) { return; }
     console.log("wOw");
     const container = document.querySelector('.cont');
     //var li = document.createElement("li");
@@ -150,8 +184,10 @@ function appendDataToPage(data) {
     divelm.appendChild(pelm);
 
     divelm.className = 'post';
-
-    pelm.textContent = data.title;
+    h1elm.textContent = 'UserID: ' + data.id; 
+    h2elm.textContent = data.title;
+    pelm.textContent = data.body;
+    
     divelm.className = 'post ';
 
     // li.appendChild(divelm);
@@ -162,7 +198,7 @@ function appendDataToPage(data) {
 }
 function appendDataToPageL(data) {
     page++;
-    if (page > 4) { return; }
+    if (page > 6) { return; }
     console.log("wow");
     const container = document.querySelector('.cont');
     //var li = document.createElement("li");
@@ -170,14 +206,16 @@ function appendDataToPageL(data) {
     var h1elm = document.createElement("h1");
     var h2elm = document.createElement("h2");
     var pelm = document.createElement("p");
-
+    h1elm.textContent = 'UserID: ' + data.id; 
     divelm.appendChild(h1elm);
     divelm.appendChild(h2elm);
     divelm.appendChild(pelm);
 
     divelm.className = 'post';
-
+    h2elm.textContent = data.title;
+    pelm.textContent = data.body;
     pelm.textContent = data.title;
+    
     divelm.className = 'post cont2';
 
    // li.appendChild(divelm);
@@ -197,7 +235,17 @@ function appendDataToPageL(data) {
     }
     if (page == 4) {
         divelm.className = 'post cont4';
-       // const target4 = document.querySelector('.cont4');
-        //observer.observe(target4);
+        const target4 = document.querySelector('.cont4');
+        observer.observe(target4);
+    }
+    if (page == 5) {
+        divelm.className = 'post cont5';
+        const target5 = document.querySelector('.cont5');
+        observer.observe(target5);
+    }
+    if (page == 6) {
+        divelm.className = 'post cont6';
+        const target6 = document.querySelector('.cont6');
+       observer.observe(target6);
     }
 }
